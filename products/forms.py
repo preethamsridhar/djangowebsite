d96a6d8239492cd1fbb5b34c48342745fd016f4f
Preetham Sridhar <preetham.prem2325@gmail.com>
2019-09-11 15:35:58 +0530

    Form validation

--- products/forms.py
+++ products/forms.py
@@ -3,6 +3,23 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={
+        "placeholder": "Your title"
+    }))
+    description = forms.CharField(
+        label='',
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "class": "my-class",
+                "rows": 20,
+                "cols": 120,
+                "placeholder": "Description"
+            }
+        ))
+    price = forms.DecimalField(initial=20.00)
+    email = forms.EmailField()
+
     class Meta:
         model = Product
         fields = [
@@ -11,18 +28,39 @@ class ProductForm(forms.ModelForm):
             'price'
         ]
 
+    def clean_title(self, *args, **kwargs):
+        title = self.cleaned_data.get("title")
+        if "CFE" in title:
+            return title
+        else:
+            raise forms.ValidationError("This is not valid title")
+
+    def clean_price(self, *args, **kwargs):
+        price = self.cleaned_data.get("price")
+        if price > 10:
+            return price
+        else:
+            raise forms.ValidationError("The price should be greater than $10")
+
+    def clean_email(self, *args, **kwargs):
+        email = self.cleaned_data.get("email")
+        if not email.endswith("edu"):
+            raise forms.ValidationError("This is not valid email")
+
 
 class RawProductForm(forms.Form):
     title = forms.CharField(label='', widget=forms.TextInput(attrs={
         "placeholder": "Your title Brother"
     }))
     description = forms.CharField(
+        label='',
         required=False,
         widget=forms.Textarea(
             attrs={
                 "class": "my-class",
                 "rows": 20,
                 "cols": 120,
+                "placeholder": "Description"
             }
         ))
     price = forms.DecimalField(initial=20.00)
--- products/views.py
+++ products/views.py
@@ -6,16 +6,17 @@ from .models import Product
 from .forms import ProductForm, RawProductForm
 from django.http import HttpResponse
 
-# # Create your views here.
-# def product_create_view(request):
-#     form = ProductForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#         form = ProductForm()
-#     context = {
-#         "form": form
-#     }
-#     return render(request, "products/product_create.html", context)
+
+# Create your views here.
+def product_create_view(request):
+    form = ProductForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = ProductForm()
+    context = {
+        "form": form
+    }
+    return render(request, "products/product_create.html", context)
 
 
 # Create your views here.
@@ -30,21 +31,21 @@ from django.http import HttpResponse
 #         return HttpResponse("Hi")
 
 
-# Create your views here.
-def product_create_view(request):
-    form = RawProductForm()
-    if request.method == "POST":
-        form = RawProductForm(request.POST)
-        if form.is_valid():
-            print(form.cleaned_data)
-            Product.objects.create(**form.cleaned_data)
-            form = RawProductForm()
-        else:
-            print(form.errors)
-    context = {
-        "form": form
-    }
-    return render(request, "products/product_create.html", context)
+# # Create your views here.
+# def product_create_view(request):
+#     form = RawProductForm()
+#     if request.method == "POST":
+#         form = RawProductForm(request.POST)
+#         if form.is_valid():
+#             print(form.cleaned_data)
+#             Product.objects.create(**form.cleaned_data)
+#             form = RawProductForm()
+#         else:
+#             print(form.errors)
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "products/product_create.html", context)
 
 
 def product_detail_view(request):
