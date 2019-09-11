8981f5d80bd555098c8de7c6908e2fe134aa9089
Preetham Sridhar <preetham.prem2325@gmail.com>
2019-09-11 14:41:19 +0530

    form widgets

--- products/forms.py
+++ products/forms.py
@@ -10,3 +10,19 @@ class ProductForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={
+        "placeholder": "Your title Brother"
+    }))
+    description = forms.CharField(
+        required=False,
+        widget=forms.Textarea(
+            attrs={
+                "class": "my-class",
+                "rows": 20,
+                "cols": 120,
+            }
+        ))
+    price = forms.DecimalField(initial=20.00)
--- products/views.py
+++ products/views.py
@@ -3,15 +3,44 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from .models import Product
-from .forms import ProductForm
+from .forms import ProductForm, RawProductForm
+from django.http import HttpResponse
+
+# # Create your views here.
+# def product_create_view(request):
+#     form = ProductForm(request.POST or None)
+#     if form.is_valid():
+#         form.save()
+#         form = ProductForm()
+#     context = {
+#         "form": form
+#     }
+#     return render(request, "products/product_create.html", context)
+
+
+# Create your views here.
+# def product_create_view(request):
+#     # print(request.GET)
+#     if request.method == "POST":
+#         print(request.POST)
+#         print("title: ", request.POST.get('title'))
+#         context = {}
+#         return render(request, "products/product_create.html", context)
+#     else:
+#         return HttpResponse("Hi")
 
 
 # Create your views here.
 def product_create_view(request):
-    form = ProductForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-        form = ProductForm()
+    form = RawProductForm()
+    if request.method == "POST":
+        form = RawProductForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            Product.objects.create(**form.cleaned_data)
+            form = RawProductForm()
+        else:
+            print(form.errors)
     context = {
         "form": form
     }
